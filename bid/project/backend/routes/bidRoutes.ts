372061eb1883b2e93e76de00b12d6b19fbaa09fa
Abhay-K-101 <abhighk2004@gmail.com>
2025-11-04 23:26:05 +0530

    Delete bid/project directory

--- bid/project/backend/routes/bidRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getBids, createBid } from "../controllers/bidController";
-const router = express.Router();
-
-router.get("/", getBids);
-router.post("/", createBid);
-
-export default router;
